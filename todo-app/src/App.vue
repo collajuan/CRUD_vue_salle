caf8df8a112264e461c1377486751726a54339b6
collajuan <collajuan@gmail.com>
2024-10-25 20:57:25 +0200

    init-todos

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,4 +1,27 @@
-<script setup>
+<script>
+export default {
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          text: "Learn JavaScript",
+          completed: false
+        },
+        {
+          id: 2,
+          text: "Build a to-do list app",
+          completed: false
+        },
+        {
+          id: 3,
+          text: "Master React",
+          completed: true
+        }
+      ]
+    }
+  }
+}
 </script>
 
 <template>
@@ -10,10 +33,14 @@
   </header>
 
   <main>
+    <div v-for="(task, index) in todos" className="todoInfo" :key="task.id">
+      <span className="todoTitle">
+        {{ task.text }}
+      </span>
+      <button className="button">Edit</button>
+      <button className="buttonDelete">Delete</button>
+    </div>
   </main>
 </template>
 
-<style scoped>
-
-
-</style>
+<style scoped></style>
