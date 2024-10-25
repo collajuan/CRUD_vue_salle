15a4cd261d0925559b1d7a5b9628b833d6aa5f6e
collajuan <collajuan@gmail.com>
2024-10-25 20:40:01 +0200

    init vue

---
+++ todo-app/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+
+    <div class="">
+      <h1>TODO App</h1>
+    </div>
+  </header>
+
+  <main>
+  </main>
+</template>
+
+<style scoped>
+
+
+</style>
